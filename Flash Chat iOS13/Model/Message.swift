05f9a24884d5e9b857b2ee94afeebeb33e13b1fc
Breno Vasconcellos <dv.brenovasconcellos@gmail.com>
2021-12-08 15:00:17 -0300

    first commit

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Breno Vasconcellos on 23/11/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
